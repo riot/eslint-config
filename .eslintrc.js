93c94eb609416e9c03e0badc60232af40b6aec93
gianlucaguarini <gianluca.guarini@gmail.com>
2023-02-10 22:41:08 +0100

    updated: config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,96 +1,41 @@
 module.exports = {
-  "root": true,
-  "plugins": [
-    "fp"
-  ],
-  "env": {
-    "browser": true,
-    "es6": true,
-    "mocha": true,
-    "node": true
+  root: true,
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['fp'],
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true,
+    node: true,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "sourceType": "module"
+  parserOptions: {
+    sourceType: 'module',
   },
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "comma-dangle": ["error", "never"],
-    "space-before-function-paren": [
-      "error",
-      "never"
-    ],
-    "space-in-parens": [
-      "error",
-      "never"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "valid-jsdoc": [
-      "warn",
+  rules: {
+    'valid-jsdoc': [
+      'warn',
       {
-        "preferType": {
-          "array": "Array",
-          "Boolean": "boolean",
-          "function": "Function",
-          "Number": "number",
-          "object": "Object",
-          "String": "string"
-        }
-      }
+        preferType: {
+          array: 'array',
+          Boolean: 'boolean',
+          function: 'function',
+          Number: 'number',
+          object: 'object',
+          String: 'string',
+        },
+      },
     ],
-    // Es6
-    "arrow-spacing": [
-      "error",
-      {
-        "before": true,
-        "after": true
-      }
-    ],
-    "no-duplicate-imports": [
-      "error",
-      {
-        "includeExports": true
-      }
-    ],
-    "no-useless-computed-key": "error",
-    "no-useless-rename": "error",
-    "prefer-const": "error",
-    "prefer-rest-params": "warn",
-    "prefer-template": "warn",
-    "rest-spread-spacing": [
-      "warn",
-      "never"
-    ],
-    "template-curly-spacing": [
-      "warn",
-      "never"
-    ],
-    "sort-imports": "warn",
-    "no-var": "error",
-    "eqeqeq": ["error", "always"],
-
     // FP options
-    "fp/no-arguments": "error",
-    "fp/no-class": "error",
-    "fp/no-delete": "error",
-    "fp/no-events": "error",
-    "fp/no-get-set": "error",
-    "fp/no-let": "error",
-    "fp/no-loops": "error",
-    "fp/no-mutating-methods": "error",
-    "fp/no-proxy": "error",
-    "fp/no-rest-parameters": "error",
-    "fp/no-valueof-field": "error"
-  }
+    'fp/no-arguments': 'error',
+    'fp/no-class': 'error',
+    'fp/no-delete': 'error',
+    'fp/no-events': 'error',
+    'fp/no-get-set': 'error',
+    'fp/no-let': 'error',
+    'fp/no-loops': 'error',
+    'fp/no-mutating-methods': 'error',
+    'fp/no-proxy': 'error',
+    'fp/no-rest-parameters': 'error',
+    'fp/no-valueof-field': 'error',
+  },
 }
-
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # eslint-config-riot
 
-[![Build Status][travis-image]][travis-url]
+[![Build Status][ci-image]][ci-url]
 [![NPM version][npm-version-image]][npm-url]
 [![NPM downloads][npm-downloads-image]][npm-url]
 [![MIT License][license-image]][license-url]
@@ -9,21 +9,26 @@ Riot default eslint config file
 
 ## Installation
 
+Peer dependencies installation
+
 ```shell
-npm i eslint-config-riot -D
+npm in -D eslint@8 eslint-plugin-fp@2 eslint-config-prettier@8 prettier@2
+```
+
+Config installation
+
+```shell
+npm i -D eslint-config-riot
 ```
 
 ## Usage
 
 Add `"extends": "eslint-config-riot"` to your `.eslintrc` file
 
-
-[travis-image]:https://img.shields.io/travis/riot/eslint-config.svg?style=flat-square
-[travis-url]:https://travis-ci.org/riot/eslint-config
-
-[license-image]:http://img.shields.io/badge/license-MIT-000000.svg?style=flat-square
-[license-url]:LICENSE.txt
-
-[npm-version-image]:http://img.shields.io/npm/v/eslint-config-riot.svg?style=flat-square
-[npm-downloads-image]:http://img.shields.io/npm/dm/eslint-config-riot.svg?style=flat-square
-[npm-url]:https://npmjs.org/package/eslint-config-riot
+[ci-image]: https://img.shields.io/github/actions/workflow/status/riot/eslint-config/test.yml?style=flat-square
+[ci-url]: https://github.com/riot/eslint-config/actions
+[license-image]: http://img.shields.io/badge/license-MIT-000000.svg?style=flat-square
+[license-url]: LICENSE.txt
+[npm-version-image]: http://img.shields.io/npm/v/eslint-config-riot.svg?style=flat-square
+[npm-downloads-image]: http://img.shields.io/npm/dm/eslint-config-riot.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/eslint-config-riot
