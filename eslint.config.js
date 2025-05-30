73177f3b433c13578eadfceb6d948bd201402218
Gianluca Guarini <gianluca.guarini@gmail.com>
2025-05-30 23:22:04 +0200

    updated: Use eslint9

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from 'eslint/config'
+import jsdoc from 'eslint-plugin-jsdoc'
+import fp from 'eslint-plugin-fp'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default defineConfig([
+  jsdoc.configs['flat/recommended'],
+  {
+    extends: compat.extends('eslint:recommended', 'prettier'),
+
+    plugins: {
+      jsdoc,
+      fp,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+        ...globals.node,
+      },
+
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      parserOptions: {},
+    },
+
+    rules: {
+      'fp/no-arguments': 'error',
+      'fp/no-class': 'error',
+      'fp/no-delete': 'error',
+      'fp/no-events': 'error',
+      'fp/no-get-set': 'error',
+      'fp/no-let': 'error',
+      'fp/no-loops': 'error',
+      'fp/no-mutating-methods': 'error',
+      'fp/no-proxy': 'error',
+      'fp/no-rest-parameters': 'error',
+      'fp/no-valueof-field': 'error',
+    },
+  },
+])
